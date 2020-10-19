29847301ac6f1d6c9dda95c0b1ebe689a8d3d75f
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-10-19 16:06:10 -0700

    [env] add dependencies and API key

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+require("dotenv").config()
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+let API_KEY = process.env.API_KEY //using nv to hide API key
+
+
 // axios call to get movies
 
 // 
