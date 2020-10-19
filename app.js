f572276f3cab826778775d4169f2da479b06e413
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-10-19 16:14:04 -0700

    [app.js, static, views] set up axios call

--- app.js
+++ app.js
@@ -6,7 +6,28 @@ const app = express();
 
 let API_KEY = process.env.API_KEY //using nv to hide API key
 
-
 // axios call to get movies
+app.set("view engine", "ejs")
+//using ejs as the view engine for rendering ejs files
+app.use(express.static("static"))
+// express using static to access CSS
+app.get('/', (req, res) => {
+    let qs = {
+        params: {
+            s: 'halloween',
+            apikey: API_KEY
+        }
+    }
+    axios.get('https://api.themoviedb.org/27/movie/horror', qs)
+    .then((response) => {
+        console.log(response.data)
+        let movies = response.data.Search //setting a variable to our data
+        res.render("home", {movies}); //render home with the data
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+app.listen(3000);
 
 // 
