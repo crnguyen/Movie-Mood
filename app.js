83c5079d0c5d488c558264143b2ffe5b4aa877ed
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-10-21 17:59:12 -0700

    [layout.ejs] add pumpkin logo and materialize

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ require("dotenv").config()
 const express = require('express');
 const axios = require('axios');
 const app = express();
-// const ejsLayouts = require('express-ejs-layouts');
+const ejsLayouts = require('express-ejs-layouts');
 const port = 3000;
 
 let API_KEY = process.env.API_KEY //using nv to hide API key
 // Enables EJS Layouts middleware
-// app.use(ejsLayouts);
+app.use(ejsLayouts);
 
 // axios call to get movies
 app.set("view engine", "ejs")
 //using ejs as the view engine for rendering ejs files
 // app.use(express.static("static"))
 // express using static to access CSS
-app.use(express.static(__dirname + '/static'));
+app.use(express.static('static'));
+// app.use(express.static(__dirname + '/static'));
 
 app.get("/", (req, res) => {
     res.render("home");
